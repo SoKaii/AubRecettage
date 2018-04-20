5d65481d4ecef16299e937850302a04678535c60
SoKaii <t.martin92500@hotmail.fr>
2018-04-20 11:28:47 +0200

    Création du Git, Vue home done

---
+++ app/src/main/java/aubervilliers/orange/aubrecettage/Home_activity.java
@@ -0,0 +1,37 @@
+package aubervilliers.orange.aubrecettage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Home_activity extends AppCompatActivity {
+
+    private TextView titreHome;
+    private ImageView backgroundBlack;
+    private Button newRecette;
+    private Button consultRecette;
+    private ImageView logoOrange;
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        // Example of a call to a native method
+        // TextView tv = (TextView) findViewById(R.id.sample_text);
+        //tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
