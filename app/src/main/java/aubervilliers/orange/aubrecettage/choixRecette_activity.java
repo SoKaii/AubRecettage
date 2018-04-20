7fa80a7c87dd1442317a933f5446f27916372972
SoKaii <t.martin92500@hotmail.fr>
2018-04-20 15:16:07 +0200

    Instanciation des éléments de la vue, et configuration du passe a choixRecette via le bouton newRecette

---
+++ app/src/main/java/aubervilliers/orange/aubrecettage/choixRecette_activity.java
@@ -0,0 +1,13 @@
+package aubervilliers.orange.aubrecettage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class choixRecette_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_choix_recette);
+    }
+}
