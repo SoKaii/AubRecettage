4c720f1b487eb0b97509d7453f4f40fe557f9c13
Argnote <ewan92@laposte.net>
2018-04-27 10:04:27 +0200

    modificaction du layout cablage simple + ajout layout J4

--- app/src/main/java/aubervilliers/orange/aubrecettage/j4_activity.java
+++ app/src/main/java/aubervilliers/orange/aubrecettage/j4_activity.java
@@ -3,11 +3,11 @@ package aubervilliers.orange.aubrecettage;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class infosTicket_activity extends AppCompatActivity {
+public class j4_activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_infos_ticket);
+        setContentView(R.layout.layout_j4);
     }
 }
